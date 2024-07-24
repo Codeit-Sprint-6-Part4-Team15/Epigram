1db80c5fd3abf8851e4d1a43f42932a7a8629077
chanho <lch0309k@gmail.com>
2024-07-24 17:52:32 +0900

    setup

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  printWidth: 200,
+  tabWidth: 2,
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindConfig: "./tailwind.config.ts",
+  importOrder: [
+    "^react",
+    "<THIRD_PARTY_MODULES>",
+    "^@/src/apis/(.*)$",
+    "^@/src/hooks/(.*)$",
+    "^@/src/app/(.*)$",
+    "^@/src/components/(.*)$",
+    "^@/src/styles/(.*)$",
+    "^[./]",
+  ],
+  importOrderSeparation: true,
+  importSortOrder: ["asc", "desc"],
+};
