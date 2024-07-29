c9f95fc0005574b745ee737112d8555ac09820b8
yeonilil <gwagseoyeon@gmail.com>
2024-07-29 10:54:27 +0900

    feat: 반응형 디자인 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,6 @@
 'use client'
 import Image from "next/image";
 import { ReactNode, useState } from "react";
-import backgroundImg from "@/public/assets/landingPage/landing-background.svg"
-import chevron from "@/public/assets/landingPage/ic-chevron-up.svg"
-import more from "@/public/assets/landingPage/ic-more-vertical.svg"
-import landingImg1 from "@/public/assets/landingPage/landing01-lg.svg"
-import landingImg2 from "@/public/assets/landingPage/landing02-lg.svg"
-import landingImg3 from "@/public/assets/landingPage/landing03-lg.svg"
-import cardImg1 from "@/public/assets/landingPage/card01-lg.svg"
-import cardImg2 from "@/public/assets/landingPage/card02-lg.svg"
-import cardImg3 from "@/public/assets/landingPage/card03-lg.svg"
-import logo2Img1 from "@/public/assets/landingPage/logo2-lg.svg"
 import Link from "next/link";
 import { motion, useAnimation, useInView } from "framer-motion"
 import { useEffect, useRef } from "react";
@@ -77,6 +67,11 @@ const ScrollWrapper = ({ children, direction }: ScrollWrapperProps) => {
 
 export default function Home() {
   const mainRef = useRef<HTMLDivElement>(null);
+  const [typingCompleted, setTypingCompleted] = useState(false);
+  const [showElements, setShowElements] = useState(false);
+  const animation = useAnimation();
+
+
   const scrollToMain = () => {
     if (mainRef.current) {
       mainRef.current.scrollIntoView({ behavior: "smooth" });
@@ -94,139 +89,247 @@ export default function Home() {
         let txt = content[i++];
         setDisplayText((prev) => prev + (txt === "\n" ? "<br/>" : txt));
         setTimeout(typing, 100); 
+      }else{
+        setShowElements(true);
       }
     };
     typing(); 
   }, []); 
 
+  useEffect(() => {
+    if (typingCompleted) {
+      animation.start("showState");
+    }
+  }, [typingCompleted, animation]);
+
   return (
     <div className="flex flex-col items-center bg-bg-100">
     <div className="w-[440px] flex flex-col items-center mt-[320px]">
-      <Image src={backgroundImg}
+      <Image src="/assets/landingPage/landing-background.svg"
       alt="배경 이미지"
       layout="fill"
       objectFit="cover"
       className="z-[1]"/>
       <div className="z-[2] flex flex-col text-center items-center">
-      <p className="iropke-4xl white-space" dangerouslySetInnerHTML={{ __html: displayText }}></p>
-      <p className="iropke-xl mt-[40px]">다른 사람들과 감정을 공유해 보세요.</p>
+      <p className="iropke-2xl md:iropke-3xl xl:iropke-4xl white-space" dangerouslySetInnerHTML={{ __html: displayText }}></p>
+      {showElements && (
+            <div className="flex flex-col items-center">
+               <ScrollWrapper direction="up">
+      <p className="iropke-md md:iropke-xl xl:iropke-xl mt-[8px] md:mt-[24px] xl:mt-[40px]">다른 사람들과 감정을 공유해 보세요.</p>
       <Link href="/">
-      <button className="w-[286px] h-[64px] mt-[48px] bg-black-500 text-white rounded-[12px]">시작하기</button>
+      <button className="w-[112px] h-[48px] mt-[24px] xl:w-[286px] xl:h-[64px] xl:mt-[48px] bg-black-500 text-white rounded-[12px]">시작하기</button>
       </Link>
-      <button onClick={scrollToMain} className="flex flex-col items-center mt-[214px] text-blue-400 text-[16px]">더 알아보기
-      <Image src={chevron}
+      </ScrollWrapper>
+      <button onClick={scrollToMain} className="flex flex-col items-center mt-[214px] text-blue-400 typo-xs-semibold md:typo-lg-medium xl:typo-lg-medium">더 알아보기
+      <Image src="/assets/landingPage/ic-chevron-up.svg"
       alt="더 알아보기 버튼"
       width={25}
       height={25}/>
       </button>
       </div>
+     
+      )}
+      </div>
     </div>
-  
+
+      
     <main ref={mainRef} className="flex flex-col text-center">
-      <div className="flex mt-[240px]">
-            <ScrollWrapper direction="left"
-          >
-      <Image src={landingImg1}
-        alt="랜딩페이지 이미지"
-        width={744}
-        height={388}/>
+      <div className="flex flex-col mt-[174px] xl:flex-row xl:mt-[240px] mr-[24px] ml-[24px]">
+            <ScrollWrapper direction="left">
+            <div className="block md:hidden xl:hidden">
+              <Image src="/assets/landingPage/landing01-sm.svg"
+                alt="랜딩페이지 이미지 (작은 크기)"
+                width={500}
+                height={250} />
+            </div>
+           <div className="hidden md:block xl:hidden">
+              <Image src="/assets/landingPage/landing01-md.svg"
+                alt="랜딩페이지 이미지 (중간 크기)"
+                width={500}
+                height={250} />
+            </div>
+            <div className="hidden xl:block">
+              <Image src="/assets/landingPage/landing01-lg.svg"
+                alt="랜딩페이지 이미지 (큰 크기)"
+                width={744}
+                height={388} />
+            </div>
         </ScrollWrapper>
-        <div className="ml-[80px] mt-[192px] text-left">
-          <p className="typo-3xl-bold">명언이나 글귀,</p>
-          <p className="typo-3xl-bold">토막 상식들을 공유해 보세요.</p>
-          <p className="mt-[40px] typo-2xl-bold text-blue-600">나만 알던 소중한 글들을</p>
-          <p className="typo-2xl-bold text-blue-600">다른 사람들에게 전파하세요.</p>
+        <div className="ml-[0px] mt-[40px] xl:ml-[80px] xl:mt-[192px] text-left">
+          <p className="typo-2xl-bold xl:typo-3xl-bold">명언이나 글귀, <br/>토막 상식들을 공유해 보세요.</p>
+          <p className="typo-lg-regualr mt-[20px] xl:mt-[40px] xl:typo-2xl-bold text-blue-600">나만 알던 소중한 글들을 
+          <br className="block md:hidden xl:block"/> 다른 사람들에게 전파하세요.</p>
       </div>
       </div>
-      <div className="flex mt-[380px]">
-        <div className="mr-[80px] mt-[192px] text-right">
-      <p className="typo-3xl-bold">감정 상태에 따라,</p>
-      <p className="typo-3xl-bold"> 알맞은 위로를 받을 수 있어요.</p>
-      <p className="typo-2xl-bold text-blue-600 mt-[40px]">
+      <div className="flex flex-col mt-[192px] md:mt-[220px] xl:mt-[380px] xl:flex-row mr-[24px] ml-[24px]">
+        <div className="mr-[80px] mt-[192px] text-right hidden xl:block">
+      <p className="typo-2xl-bold xl:typo-3xl-bold">감정 상태에 따라, <br/> 알맞은 위로를 받을 수 있어요.</p>
+      <p className="typo-lg-regualr mt-[20px] xl:mt-[40px] xl:typo-2xl-bold text-blue-600">
         태그를 통해 글을 모아 볼 수 있어요.
         </p>
         </div>
         <ScrollWrapper direction="right"
           >
-        <Image src={landingImg2}
-      alt="랜딩페이지 이미지"
-      width={744}
-      height={388}/>
+       <div className="block md:hidden xl:hidden ">
+              <Image src="/assets/landingPage/landing02-sm.svg"
+                alt="랜딩페이지 이미지 (작은 크기)"
+                width={500}
+                height={250} />
+            </div>
+           <div className="hidden md:block xl:hidden">
+              <Image src="/assets/landingPage/landing02-md.svg"
+                alt="랜딩페이지 이미지 (중간 크기)"
+                width={500}
+                height={250} />
+            </div>
+            <div className="hidden xl:block">
+              <Image src="/assets/landingPage/landing02-lg.svg"
+                alt="랜딩페이지 이미지 (큰 크기)"
+                width={744}
+                height={388} />
+            </div>
       </ScrollWrapper>
-      </div>
-      <div className="flex mt-[380px]">
-      <ScrollWrapper direction="left"
-          >
-      <Image src={landingImg3}
-      alt="랜딩페이지 이미지"
-      width={744}
-      height={388}/>
-      </ScrollWrapper>
-      <div className="ml-[80px] mt-[192px] text-left">
-      <p className="typo-3xl-bold">
-        내가 요즘 어떤 감정 상태인지 </p>
-        <p className="typo-3xl-bold">통계로 한눈에 볼 수 있어요.
-        </p>
-        <p className="typo-2xl-bold text-blue-600 mt-[40px]">
-        감정 달력으로</p>
-        <p className="typo-2xl-bold text-blue-600">내 마음에 담긴 감정을 확인해보세요
+      <div className="mt-[40px] xl:mt-[192px] text-right block md:block xl:hidden">
+      <p className="typo-2xl-bold xl:typo-3xl-bold">감정 상태에 따라, <br/> 알맞은 위로를 받을 수 있어요.</p>
+      <p className="typo-lg-regualr mt-[20px] xl:mt-[40px] xl:typo-2xl-bold text-blue-600">
+        태그를 통해 글을 모아 볼 수 있어요.
         </p>
         </div>
       </div>
-      <div className="mt-[480px] text-center flex flex-col items-center mb-[60px]">
-      <p className="typo-3xl-bold">사용자들이 직접</p>
-      <p className="typo-3xl-bold"> 인용한 에피그램들</p>
-      <ScrollWrapper direction="up"
-          >
-      <Image src={cardImg1}
-      alt="랜딩페이지 이미지"
-      width={640}
-      height={388}
-      className="mt-[100px]"/>
+      <div className="flex flex-col mt-[192px] md:mt-[220px] xl:mt-[380px] xl:flex-row mr-[24px] ml-[24px]">
+      <ScrollWrapper direction="left">
+      <div className="block md:hidden xl:hidden">
+              <Image src="/assets/landingPage/landing03-sm.svg"
+                alt="랜딩페이지 이미지 (작은 크기)"
+                width={500}
+                height={250} />
+            </div>
+           <div className="hidden md:block xl:hidden">
+              <Image src="/assets/landingPage/landing03-md.svg"
+                alt="랜딩페이지 이미지 (중간 크기)"
+                width={500}
+                height={250} />
+            </div>
+            <div className="hidden xl:block">
+              <Image src="/assets/landingPage/landing03-lg.svg"
+                alt="랜딩페이지 이미지 (큰 크기)"
+                width={744}
+                height={388} />
+            </div>
+      </ScrollWrapper>
+      <div className="ml-[0px] xl:ml-[80px] mt-[40px] xl:mt-[192px] text-left">
+      <p className="typo-2xl-bold xl:typo-3xl-bold">내가 요즘 어떤 감정 상태인지 <br/>통계로 한눈에 볼 수 있어요.</p>
+        <p className="typo-lg-regualr mt-[20px] xl:mt-[40px] xl:typo-2xl-bold text-blue-600">
+        감정 달력으로 <br className="block md:hidden xl:block"/>내 마음에 담긴 감정을 확인해보세요</p>
+        </div>
+      </div>
+      <div className="mt-[280px] xl:mt-[480px] text-center flex flex-col items-center">
+      <p className="typo-2xl-bold xl:typo-3xl-bold mb-[40px] xl:mb-[60px]">사용자들이 직접 <br/>인용한 에피그램들</p>
+      <ScrollWrapper direction="up">
+      <div className="block md:hidden xl:hidden">
+              <Image src="/assets/landingPage/card01-sm.svg"
+                alt="카드 이미지 (작은 크기)"
+                width={312}
+                height={152} />
+            </div>
+           <div className="hidden md:block xl:hidden">
+              <Image src="/assets/landingPage/card01-md.svg"
+                alt="카드 이미지 (중간 크기)"
+                width={384}
+                height={180} />
+            </div>
+            <div className="hidden xl:block">
+              <Image src="/assets/landingPage/card01-lg.svg"
+                alt="카드 이미지 (큰 크기)"
+                width={640}
+                height={198} />
+            </div>
       </ScrollWrapper>
       <ScrollWrapper direction="up"
           >
-       <Image src={cardImg2}
-      alt="랜딩페이지 이미지"
-      width={640}
-      height={388}
-      className="mt-[60px]"/>
+         <div className="block md:hidden xl:hidden mt-[16]">
+              <Image src="/assets/landingPage/card02-sm.svg"
+                alt="카드 이미지 (작은 크기)"
+                width={312}
+                height={200} />
+            </div>
+           <div className="hidden md:block xl:hidden mt-[20]">
+              <Image src="/assets/landingPage/card02-md.svg"
+                alt="카드 이미지 (중간 크기)"
+                width={384}
+                height={232} />
+            </div>
+            <div className="hidden xl:block mt-[60]">
+              <Image src="/assets/landingPage/card02-lg.svg"
+                alt="카드 이미지 (큰 크기)"
+                width={640}
+                height={276} />
+            </div>
       </ScrollWrapper>
       <ScrollWrapper direction="up"
           >
-       <Image src={cardImg3}
-      alt="랜딩페이지 이미지"
-      width={640}
-      height={388}
-      className="mt-[60px]"/>
+         <div className="block md:hidden xl:hidden mt-[16]">
+              <Image src="/assets/landingPage/card03-sm.svg"
+                alt="카드 이미지 (작은 크기)"
+                width={312}
+                height={152} />
+            </div>
+           <div className="hidden md:block xl:hidden mt-[20]">
+              <Image src="/assets/landingPage/card03-md.svg"
+                alt="카드 이미지 (중간 크기)"
+                width={384}
+                height={180} />
+            </div>
+            <div className="hidden xl:block mt-[60]">
+              <Image src="/assets/landingPage/card03-lg.svg"
+                alt="카드 이미지 (큰 크기)"
+                width={640}
+                height={196} />
+            </div>
            </ScrollWrapper>
            <ScrollWrapper direction="up"
           >
-       <Image src={more}
+       <Image src="assets/landingPage/ic-more-vertical.svg"
       alt="더 알아보기 버튼"
       width={25}
       height={25}
-      className="mt-[40px]"/>
+      className="mt-[40px] mb-[30px] xl:mb-[60px]"/>
           </ScrollWrapper>
         </div>
         
     </main>
     <div className="relative w-screen">
     <div className="flex flex-col items-center  relative z-[2]">
-    <Image src={logo2Img1}
+      <div className="block md:hidden xl:hidden">
+      <Image src="/assets/landingPage/logo2-lg.svg"
+      alt="날마다 에피그램"
+      width={122}
+      height={200}
+      className="mt-[180px]"/>
+            </div>
+            <div className="hidden md:block xl:hidden">
+      <Image src="/assets/landingPage/logo2-lg.svg"
+      alt="날마다 에피그램"
+      width={122}
+      height={200}
+      className="mt-[180px]"/>
+            </div>
+       <div className="hidden md:hidden xl:block">
+      <Image src="/assets/landingPage/logo2-xl.svg"
       alt="날마다 에피그램"
       width={184}
       height={388}
       className="mt-[495px]"/>
+            </div>
         <Link href="/">
-    <button className="w-[286px] h-[64px] mt-[48px] bg-black-500 text-white rounded-[12px] mb-[400px]">시작하기</button>
+    <button className="typo-lg-semibold w-[112px] h-[48px] mt-[24px] xl:w-[286px] xl:h-[64px] xl:mt-[48px] bg-black-500 text-white rounded-[12px] mb-[200px] xl:mb-[400px]">시작하기</button>
     </Link>
     </div>
-    <Image src={backgroundImg}
+    <Image src="/assets/landingPage/landing-background.svg"
       alt="배경 이미지"
       layout="fill"
       objectFit="cover"
-      className=" b-0 w-screen z-[1]"/>
+      className=" w-screen z-[1]"/>
     </div>
     </div>
   );
