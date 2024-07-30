72d2d270349e3409789d4c1a6527874d5cd81040
Lee Youl <2yulrang@gmail.com>
2024-07-30 17:31:56 +0900

    feat: ProfileModal 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,18 @@ import { useState } from "react";
 import Modal from "@/src/components/commons/Modal/Modal";
 import useModal from "@/src/hooks/useModal";
 import ConfirmModal from "@/src/components/commons/Modal/ConfirmModal";
+import ProfileModal from "@/src/components/commons/Modal/ProfileModal";
+import IcoUser from "@/public/assets/ic_user.svg";
+
+const writer = {
+  nickname: "지킬과 하이드",
+  image: IcoUser,
+  id: 1,
+};
 
 export default function Home() {
   const [isDeleteModalOpened, { open: openDeleteModal, close: closeDeleteModal }] = useModal(false);
+  const [isProfileModalOpened, { open: openProfileModal, close: closeProfileModal }] = useModal(false);
 
   const handleDelete = () => {
     alert(1);
@@ -14,9 +23,18 @@ export default function Home() {
 
   return (
     <>
-      <button onClick={openDeleteModal}>버튼</button>
+      <div>
+        <button onClick={openDeleteModal}>삭제 모달 열기</button>
+      </div>
+      <div>
+        <button onClick={openProfileModal}>프로필 모달 열기</button>
+      </div>
+
       <Modal opened={isDeleteModalOpened}>
         <ConfirmModal message="댓글을 삭제하시겠어요?" btnText="삭제하기" onClose={closeDeleteModal} onSubmit={handleDelete} />
+      </Modal>
+      <Modal opened={isProfileModalOpened}>
+        <ProfileModal writer={writer} onClose={closeProfileModal} />
       </Modal>
     </>
   );
