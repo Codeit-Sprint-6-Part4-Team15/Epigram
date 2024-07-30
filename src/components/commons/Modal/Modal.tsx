9fb34a58a3f8ba79decca35b213a0b36a9cd3df8
Lee Youl <2yulrang@gmail.com>
2024-07-30 14:53:18 +0900

    refactor: 필요없는 코드 삭제

--- src/components/commons/Modal/Modal.tsx
+++ src/components/commons/Modal/Modal.tsx
@@ -2,11 +2,10 @@ import { ReactNode } from "react";
 
 interface ModalProps {
   opened: boolean;
-  onClose: () => void;
   children: ReactNode;
 }
 
-export default function Modal({ opened, onClose, children }: ModalProps) {
+export default function Modal({ opened, children }: ModalProps) {
   if (opened) {
     return (
       <div className="fixed top-0 flex h-lvh w-full items-center justify-center">
