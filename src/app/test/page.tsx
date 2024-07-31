48ccf368a2d5e533f945097b4292eca9ba7bd7c3
yeonilil <gwagseoyeon@gmail.com>
2024-07-31 14:14:03 +0900

    feat: 드롭다운 선택값 저장 반영

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,9 +1,15 @@
-import Dropdown from "@/src/components/commons/Dropdown"
+"use client";
+
+import Dropdown from "@/src/components/commons/Dropdown";
+import { useState } from "react";
+
 
 export default function Page() {
+    const [selectedValue, setSelectedValue] = useState("필터: 없음");
+
     return (
         <div>
-            <Dropdown/>
+             <Dropdown selectedValue={selectedValue} setSelectedValue={setSelectedValue} />
         </div>
     )
   }
