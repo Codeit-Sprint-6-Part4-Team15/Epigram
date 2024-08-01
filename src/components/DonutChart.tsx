6548168dc3975e59a49105255c88646b0290ea34
Lee Youl <2yulrang@gmail.com>
2024-08-01 23:23:39 +0900

    fix: DonutChart 값 수정

--- src/components/DonutChart.tsx
+++ src/components/DonutChart.tsx
@@ -85,9 +85,9 @@ export default function DonutChart({ data }: DonutChartProps) {
             strokeLinejoin="round"
             strokeLinecap="round"
             d={`M ${getCoordFromDegrees(SPACE_LENGTH, 50 - STROKE_WIDTH / 2)}
-                A ${50 - STROKE_WIDTH / 2} ${50 - STROKE_WIDTH / 2} 0 ${item.deg > 50 ? 1 : 0} 0 ${getCoordFromDegrees(item.deg - SPACE_LENGTH, 50 - STROKE_WIDTH / 2)}
+                A ${50 - STROKE_WIDTH / 2} ${50 - STROKE_WIDTH / 2} 0 ${item.rate > 50 ? 1 : 0} 0 ${getCoordFromDegrees(item.deg - SPACE_LENGTH, 50 - STROKE_WIDTH / 2)}
                 L ${getCoordFromDegrees(item.deg - SPACE_LENGTH, 50 - FACE_WIDTH - STROKE_WIDTH / 2)}
-                A ${50 - FACE_WIDTH - STROKE_WIDTH / 2} ${50 - FACE_WIDTH - STROKE_WIDTH / 2} 0 ${item.deg > 50 ? 1 : 0} 1 ${getCoordFromDegrees(SPACE_LENGTH, 50 - FACE_WIDTH - STROKE_WIDTH / 2)}`}
+                A ${50 - FACE_WIDTH - STROKE_WIDTH / 2} ${50 - FACE_WIDTH - STROKE_WIDTH / 2} 0 ${item.rate > 50 ? 1 : 0} 1 ${getCoordFromDegrees(SPACE_LENGTH, 50 - FACE_WIDTH - STROKE_WIDTH / 2)}`}
           />
         );
       }
