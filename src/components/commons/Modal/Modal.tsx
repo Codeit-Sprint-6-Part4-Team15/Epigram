b603e3be0c2b5c37aef3c4f6242c33b0d8ba2e72
Lee Youl <2yulrang@gmail.com>
2024-08-02 23:26:14 +0900

    feat: Modal 컴포넌트에 Portal 적용

--- src/components/commons/Modal/Modal.tsx
+++ src/components/commons/Modal/Modal.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
+import { createPortal } from "react-dom";
 
 interface ModalProps {
   opened: boolean;
@@ -6,14 +7,29 @@ interface ModalProps {
 }
 
 export default function Modal({ opened, children }: ModalProps) {
+  const [modalRoot, setModalRoot] = useState<Element | null>(null);
+
+  useEffect(() => {
+    const root = document.getElementById('modal-root');
+    setModalRoot(root);
+  }, []);
+
+  if (!modalRoot) {
+    return null; // modalRoot가 준비되지 않은 경우 아무것도 렌더링하지 않음
+  }
+
   if (opened) {
     return (
-      <div className="fixed top-0 flex h-lvh w-full items-center justify-center">
+      <>
+      { createPortal(
+      <div className="fixed z-10 top-0 flex h-lvh w-full items-center justify-center">
         <div className="bg-dim h-full w-full"></div>
         <dialog open className="absolute bg-transparent">
           {children}
         </dialog>
       </div>
+      , modalRoot)}
+      </>
     );
   }
 }
