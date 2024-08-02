820c3c74405feed6a80ce1a55e10cfd44b38718b
Lee Youl <2yulrang@gmail.com>
2024-08-02 23:33:13 +0900

    merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use client'
 import Image from "next/image";
 import { ReactNode, useState } from "react";
@@ -47,47 +46,6 @@ const ScrollWrapper = ({ children, direction }: ScrollWrapperProps) => {
     >
       {children}
     </motion.div>
-=======
-"use client";
-import { useState } from "react";
-import Modal from "@/src/components/commons/Modal/Modal";
-import useModal from "@/src/hooks/useModal";
-import ConfirmModal from "@/src/components/commons/Modal/ConfirmModal";
-import ProfileModal from "@/src/components/commons/Modal/ProfileModal";
-
-// FIX: API 연결 후 삭제
-const writer = {
-  nickname: "지킬과 하이드",
-  image: null,
-  id: 1,
-};
-
-export default function Home() {
-  const [isDeleteModalOpened, { open: openDeleteModal, close: closeDeleteModal }] = useModal(false);
-  const [isProfileModalOpened, { open: openProfileModal, close: closeProfileModal }] = useModal(false);
-
-  const handleDelete = () => {
-    alert(1);
-    closeDeleteModal();
-  };
-
-  return (
-    <>
-      <div>
-        <button onClick={openDeleteModal}>삭제 모달 열기</button>
-      </div>
-      <div>
-        <button onClick={openProfileModal}>프로필 모달 열기</button>
-      </div>
-
-      <Modal opened={isDeleteModalOpened}>
-        <ConfirmModal message="댓글을 삭제하시겠어요?" btnText="삭제하기" onClose={closeDeleteModal} onSubmit={handleDelete} />
-      </Modal>
-      <Modal opened={isProfileModalOpened}>
-        <ProfileModal writer={writer} onClose={closeProfileModal} />
-      </Modal>
-    </>
->>>>>>> 50334a7dfbf58140f5a2f96673b9ef339e24a815
   );
 };
 
