6b11924b26525f83c8f829113819c329357e8ae1
Lee Youl <2yulrang@gmail.com>
2024-08-05 21:06:18 +0900

    feat: 댓글 더보기 기능 구현

--- src/app/api/axios.ts
+++ src/app/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_URL = process.env.NEXT_PUBLIC_API;
 
-const ACCESS_TOKKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTM2LCJ0ZWFtSWQiOiI2LTE1Iiwic2NvcGUiOiJhY2Nlc3MiLCJpYXQiOjE3MjI4NTUwMDYsImV4cCI6MTcyMjg1NjgwNiwiaXNzIjoic3AtZXBpZ3JhbSJ9.BARmvaxbGEHR9ityl2x-DjIgaRm_NrXLJfZzU346DeA";
+const ACCESS_TOKKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTM2LCJ0ZWFtSWQiOiI2LTE1Iiwic2NvcGUiOiJhY2Nlc3MiLCJpYXQiOjE3MjI4NTcwNjcsImV4cCI6MTcyMjg1ODg2NywiaXNzIjoic3AtZXBpZ3JhbSJ9.xJPbgO2DPGDDE1RkRU3A5oMZoMKPa_xWkUoEqchmE3g";
 const instance = axios.create({
   baseURL: API_URL,
   timeout: 5000,
