d7fe918c6fbc7e7977b0334ecb8344586b4e42f1
Lee Youl <2yulrang@gmail.com>
2024-08-05 20:17:15 +0900

    feat: 댓글 불러오기, 수정, 삭제기능 추가

--- src/app/api/axios.ts
+++ src/app/api/axios.ts
@@ -2,12 +2,14 @@ import axios from 'axios';
 
 const API_URL = process.env.NEXT_PUBLIC_API;
 
+const ACCESS_TOKKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTM2LCJ0ZWFtSWQiOiI2LTE1Iiwic2NvcGUiOiJhY2Nlc3MiLCJpYXQiOjE3MjI4NTUwMDYsImV4cCI6MTcyMjg1NjgwNiwiaXNzIjoic3AtZXBpZ3JhbSJ9.BARmvaxbGEHR9ityl2x-DjIgaRm_NrXLJfZzU346DeA";
 const instance = axios.create({
   baseURL: API_URL,
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
+    'Authorization': `Bearer ${ACCESS_TOKKEN}`
   },
 });
 
--- src/components/CommentContainer.tsx
+++ src/components/CommentContainer.tsx
@@ -1,59 +1,20 @@
 "use client";
 
 import { CommentType } from "@/src/types";
-import instance from "../app/api/axios";
 import Comment from "./commons/Comment";
-import { useEffect } from "react";
-
-let comments : CommentType[] = [];
-
-async function getRecentComments() {
-  try {
-    const res = await instance.get("comments", {
-      params: {
-        limit : 4
-      }
-    });
-    comments = await res.data.list;
-  } catch (error) {
-    throw new Error("댓글을 수정하는데 실패했습니다.")
-  }
-  return comments;
-}
-
-async function handleEdit (id:number, content:string, isPublic:boolean) {
-  try {
-    const res = await instance.patch("comments", {
-      params: {
-        id,
-        content,
-        isPublic
-      }
-    });
-  } catch (error) {
-    throw new Error("댓글을 수정하는데 실패했습니다.")
-  }
-}
-
-async function handleDelete (id:number) {
-  try {
-    const res = await instance.delete("comments", {
-      params: {
-        id
-      }
-    });
-  } catch (error) {
-    throw new Error("댓글을 수정하는데 실패했습니다.")
-  }
-}
+import { getRecentComments, handleCommentDelete, handleCommentEdit } from "../app/api/comment";
 
 export default async function CommentsContainer() {
-  comments = await getRecentComments();
+  let comments : CommentType[] | null = null;
+
+  if( !comments ) {
+    comments = await getRecentComments();
+  }
 
   return (
     <div className="space-y-4">
-      {comments.map(comment => (
-        <Comment comment={comment} onEdit={handleEdit} onDelete={handleDelete}/>
+      {comments?.map(comment => (
+        <Comment comment={comment} onEdit={handleCommentEdit} onDelete={handleCommentDelete} />
       ))}
     </div>
   );
