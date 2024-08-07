bda451789bfe69959c3b338f88d3860e19dfff60
곽서연 <120254101+yeonilil@users.noreply.github.com>
2024-08-07 20:48:26 +0900

    Feat/epi 29/write epigram (#19)
    * feat: 랜딩페이지 UI
    
    * feat: framer-motion 애니메이션 추가, 타이핑 효과 추가
    
    * feat: 반응형 디자인 추가
    
    * style: 스크롤 바 커스텀
    
    * fix: 이미지 형식 webp로 변환
    
    * branch
    
    * feat: 드롭다운 구현
    
    * feat: 드롭다운 선택값 저장 반영
    
    * style: 글 작성 페이지 공란일때 스타일 구현
    
    * fix:유효성 검사 추가
    
    * fix: 내용, 저자 란 에러 처리, TextArea 컴포넌트 register 연결
    
    * feat: 글 작성 구현, API 연결
    
    * feat: 태그 3개 초과, 저자, 내용 미입력시 Toast 구현

--- src/types/epigrams.d.ts
+++ src/types/epigrams.d.ts
@@ -21,3 +21,19 @@ export interface TextCardProps {
   tags: EpigramTag[];
   id: number;
 }
+
+export interface epigramsParams {
+    id?: number;
+    limit?: number;
+    cursor?: number;
+    keyword?: string;
+    writerId?: number;
+  }
+
+  export interface PostEpigramData {
+    tags?: string[];
+    referenceUrl?: string;
+    referenceTitle?: string;
+    author?: string;
+    content?: string;
+  }
