732c55d0b6ba7b25f6463339b2e5ecfc2e4cd712
Lee Youl <2yulrang@gmail.com>
2024-08-09 14:33:28 +0900

    feat: 오늘의 에피그램 추가(222번 에피그램)

--- src/app/epigrams/page.tsx
+++ src/app/epigrams/page.tsx
@@ -1,10 +1,43 @@
+import instance from '@/src/app/api/axios';
+
 import CommentsContainer from '@/src/components/CommentContainer';
 import EpigramsContainer from '@/src/components/EpigramsContainer';
+import TextCard from '@/src/components/commons/TextCard';
 
+async function getTodayEpigram() {
+  let todayEpigramData;
+  try {
+    //TODO : const res = await instance.get('epigrams/today');
+    const res = await instance.get('epigrams/222');
+    todayEpigramData = await res.data;
+  } catch (error) {
+    console.error('오늘의 에피그램 데이터를 불러오는데 실패했습니다.');
+  }
+  return todayEpigramData;
+}
 
-export default function EpigramsPage() {
+export default async function EpigramsPage() {
+  const todayEpigram = await getTodayEpigram();
+
   return (
     <div className="flex min-h-[100vh] flex-col items-center bg-bg-100">
+      <div className="w-[384px] py-[36px] xl:w-[640px] xl:py-[80px]">
+        <h3 className="typo-lg-semibold mb-[16px] text-black-600 xl:typo-2xl-semibold xl:mb-[40px]">
+          오늘의 에피그램
+        </h3>
+        <TextCard
+          id={todayEpigram?.id}
+          content={todayEpigram?.content}
+          author={todayEpigram?.author}
+          tags={todayEpigram?.tags ?? []}
+        />
+      </div>
+      <div className="w-[384px] py-[36px] xl:w-[640px] xl:py-[80px]">
+        <h3 className="typo-lg-semibold mb-[16px] text-black-600 xl:typo-2xl-semibold xl:mb-[40px]">
+          오늘의 감정은 어떤가요?
+        </h3>
+        <div>컴포넌트</div>
+      </div>
       <div className="w-[384px] py-[36px] xl:w-[640px] xl:py-[80px]">
         <h3 className="typo-lg-semibold mb-[16px] text-black-600 xl:typo-2xl-semibold xl:mb-[40px]">
           최신 에피그램
