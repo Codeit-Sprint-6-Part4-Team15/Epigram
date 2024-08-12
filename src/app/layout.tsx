8d46ce4b063f49fdeb19f6afbf5829e12b08eb79
Dongseok Lee <ehdtjr510@naver.com>
2024-08-12 16:50:15 +0900

    feat: layout.tsx에 header 컴포넌트 추가 및 스타일 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 import './globals.css';
+import Header from './Header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
     <html lang="ko">
       <body>
         <div>
+          <Header />
           <ToastContainer autoClose={2000} />
           {children}
         </div>
