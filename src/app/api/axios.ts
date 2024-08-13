136ef77d611f16d3ad3debb398248f5e51b0dc5e
leeyoul <2yulrang@gmail.com>
2024-08-13 14:08:37 +0900

    Feat/epi 2/button : 토글 버튼 동작수정 (#36)
    * feat: RadioGroup 컴포넌트 추가
    
    * feat: Toggle 버튼 추가
    
    * fix: 전역스타일 정리
    
    * chore: 코드 정리
    
    * feat: RadioGroup 컴포넌트 추가
    
    * feat: Toggle 버튼 추가
    
    * fix: 전역스타일 정리
    
    * chore: 코드 정리
    
    * fix: RadioGroup 컴포넌트 prop value 형태 수정
    
    * fix: Toggle 컴포넌트 prop value 형태 수정
    
    * fix: 충돌 제거
    
    * fix: Button width full로 수정
    
    * feat : Button style prop 추가
    
    * fix: 토글 버튼 동작 수정

--- src/app/api/axios.ts
+++ src/app/api/axios.ts
@@ -8,7 +8,7 @@ const API_URL = process.env.NEXT_PUBLIC_API;
 
 //TODO: access token 수정 필요
 const ACCESS_TOKKEN =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTM2LCJ0ZWFtSWQiOiI2LTE1Iiwic2NvcGUiOiJhY2Nlc3MiLCJpYXQiOjE3MjMyNzA5NjcsImV4cCI6MTcyMzI3Mjc2NywiaXNzIjoic3AtZXBpZ3JhbSJ9.NWZPPh1Z-fynb_H5mtxucluxR33oWjgrsGsu0R-zNdQ';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTM2LCJ0ZWFtSWQiOiI2LTE1Iiwic2NvcGUiOiJhY2Nlc3MiLCJpYXQiOjE3MjM1MjI5ODMsImV4cCI6MTcyMzUyNDc4MywiaXNzIjoic3AtZXBpZ3JhbSJ9.S7SCAo6DMICxd-tbGE83Vs3n-Gn3MNH6bFiuih2GLZU';
 const instance = axios.create({
   baseURL: API_URL,
   timeout: 5000,
