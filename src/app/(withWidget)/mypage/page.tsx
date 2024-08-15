c8a74b51dde779759808ace2e5921f10a92ac1a7
chanho <lch0309k@gmail.com>
2024-08-16 01:54:55 +0900

    feat: mypage 오늘의 감정 날짜 추가

--- src/app/(withWidget)/mypage/page.tsx
+++ src/app/(withWidget)/mypage/page.tsx
@@ -4,7 +4,19 @@ import MyContents from '@/src/components/MyContents';
 import UserInfo from '@/src/components/UserInfo';
 import TodayEmotionSelector from '@/src/components/commons/TodayEmotionSelector';
 
+function formatDate(date: Date): string {
+  return new Intl.DateTimeFormat('ko-KR', {
+    year: '2-digit',
+    month: '2-digit',
+    day: '2-digit',
+  })
+    .format(date)
+    .replace(/\.\s/g, '.')
+    .slice(0, -1);
+}
+
 export default function MyPage() {
+  const today = formatDate(new Date());
   return (
     <div className="flex min-h-[100vh] flex-col items-center bg-bg-100 pt-[64px] xl:pt-[128px]">
       <div className="shadow-1 flex w-full flex-col items-center rounded-[24px] bg-white">
@@ -12,9 +24,14 @@ export default function MyPage() {
           <UserInfo />
         </div>
         <div className="w-[384px] py-[36px] xl:w-[640px] xl:py-[80px]">
-          <h3 className="typo-lg-semibold mb-[16px] text-black-600 xl:typo-2xl-semibold xl:mb-[40px]">
-            오늘의 감정
-          </h3>
+          <div className="mb-[16px] flex items-center justify-between">
+            <h3 className="typo-lg-semibold text-black-600 xl:typo-2xl-semibold xl:mb-[40px]">
+              오늘의 감정
+            </h3>
+            <span className="typo-lg-regular text-gray-400 xl:typo-xl-regular">
+              {today}
+            </span>
+          </div>
           <div className="mb-[56px] flex justify-center lg:mb-[60px] xl:mb-[164px]">
             <TodayEmotionSelector userId={136} />
           </div>
